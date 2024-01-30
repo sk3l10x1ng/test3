b51f50d848f422ca4765633e7442b19d51e27611
sk3l10x1ng <mr4obot@gmail.com>
2024-01-30 23:16:34 +0530

    test

---
+++ _site/test/jekyll-theme-chirpy/assets/js/data/swconf.js
@@ -0,0 +1,34 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1706636740',resources: [
+      '/test3/assets/css/jekyll-theme-chirpy.css',
+      '/test3/',
+      
+        '/test3/categories/',
+      
+        '/test3/tags/',
+      
+        '/test3/archives/',
+      
+        '/test3/about/',
+      
+
+      
+      
+    ],allowHosts: [
+      
+        'chirpy-img.netlify.app',
+      
+          'fonts.googleapis.com',
+          'fonts.gstatic.com',
+          'fonts.googleapis.com',
+          'cdn.jsdelivr.net',
+          'polyfill.io'
+      
+    ],denyPaths: [
+      
+    ],
+    purge: false
+  
+};
+
